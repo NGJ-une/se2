867a416369a8bf7f2060f09ef46d034f1970f0cf
jdy7149 <jdy100000@gmail.com>
2025-04-03 10:27:25 +0900

    Add: implemented searchRooms in ReserDAO

--- se2/src/main/java/com/hotel/reser/ReserDAO.java
+++ se2/src/main/java/com/hotel/reser/ReserDAO.java
@@ -18,13 +18,12 @@ public class ReserDAO {
 			
 			conn = com.hotel.db.HotelDB.getConn();
 			
-			String sql = "SELECT FROM hotel WHERE name = ?";
+			String sql = "SELECT * FROM hotel WHERE name = ?";
 			ps = conn.prepareStatement(sql);
 			ps.setString(1, location);
 			
 			rs = ps.executeQuery();
 			
-			// Get max count of rooms for selected location
 			HashMap<String, Integer> roomMaxCount = new HashMap<String, Integer>();
 			if (rs.next()) {
 				roomMaxCount.put("standard", rs.getInt("standard"));
@@ -33,7 +32,6 @@ public class ReserDAO {
 				roomMaxCount.put("grand", rs.getInt("grand"));
 			}
 			
-			// Get current count of rooms for selected location
 			sql = "SELECT SUBSTR(type, 3) AS room_type, COUNT(type) AS cur_count FROM reser WHERE type LIKE ? GROUP BY type";
 			ps = conn.prepareStatement(sql);
 			ps.setString(1, locaInitial + "_");
@@ -44,12 +42,35 @@ public class ReserDAO {
 			while (rs.next()) {
 				roomCurCount.put(rs.getString("room_type"), rs.getInt("cur_count"));
 			}
+			
+			sql = "SELECT * FROM room WHERE type LIKE ? AND capacity >= ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, locaInitial + "_");
+			ps.setInt(2, headCount);
+			
+			rs = ps.executeQuery();
+			
+			ArrayList<RoomDTO> res = new ArrayList<RoomDTO>();
+			while (rs.next()) {
+				String roomType = rs.getString("type").substring(2);
+				int maxCount = roomMaxCount.get(roomType);
+				int curCount = roomCurCount.getOrDefault(roomType, 0);
+				
+				if (curCount < maxCount) {
+					RoomDTO dto = new RoomDTO(rs.getString("type"), rs.getInt("idx"), rs.getString("img_name"), rs.getInt("price"),
+							rs.getInt("capacity"));
+					res.add(dto);
+				}
+			}
+			return res;
 		} catch(Exception e){
 			e.printStackTrace();
 			return null;
 		} finally {
 			try {
-				
+				if (rs != null) rs.close();
+				if (ps != null) ps.close();
+				if (conn != null) conn.close();
 			} catch(Exception e) {}
 		}
 	}
